186b9c4bd2f7c6077271dfd97771384802b5133a
Filip <f.kaczmarek1@wp.pl>
2021-07-19 18:20:45 +0200

    Validation if array is empty

--- src/Components/ToDoList.js
+++ src/Components/ToDoList.js
@@ -10,7 +10,13 @@ export class ToDoList extends React.Component {
         return (
             <div className={'todo-list__container'}>
                 <ul className={'todo-list__list'}>
-                    {tasks.map((task, keyID) => {
+                    {tasks.length === 0 
+                    ? 
+                    <li key={'empty-list'}
+                        className={'empty-list'}
+                    >List has no items</li> 
+                    :
+                    tasks.map((task, keyID) => {
                         return (<li key={task + Math.random()} className={'todo-list__list-item'}>
                             <span>{task.text}</span> 
                             <button><FaTrashAlt /></button>
